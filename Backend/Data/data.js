6d231d527224016a3a4ae596cc5dbada0c7ed025
saketbhatnagar1 <165547660+saketbhatnagar1@users.noreply.github.com>
2024-06-20 20:05:47 +0530

    Add files via upload

---
+++ Backend/Data/data.js
@@ -0,0 +1,93 @@
+const Chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "JOHN DOE",
+        email: "john.doe@example.com",
+      },
+      {
+        name: "JANE SMITH",
+        email: "jane.smith@example.com",
+      },
+    ],
+    _id: "001",
+    chatName: "JOHN DOE",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "ALICE JOHNSON",
+        email: "alice.johnson@example.com",
+      },
+      {
+        name: "BOB BROWN",
+        email: "bob.brown@example.com",
+      },
+    ],
+    _id: "002",
+    chatName: "ALICE JOHNSON",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "CHARLIE BLACK",
+        email: "charlie.black@example.com",
+      },
+      {
+        name: "DAVE WHITE",
+        email: "dave.white@example.com",
+      },
+    ],
+    _id: "003",
+    chatName: "CHARLIE BLACK",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "EMILY GREEN",
+        email: "emily.green@example.com",
+      },
+      {
+        name: "FRANK BLUE",
+        email: "frank.blue@example.com",
+      },
+    ],
+    _id: "004",
+    chatName: "EMILY GREEN",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "GEORGE RED",
+        email: "george.red@example.com",
+      },
+      {
+        name: "HANNAH YELLOW",
+        email: "hannah.yellow@example.com",
+      },
+    ],
+    _id: "005",
+    chatName: "GEORGE RED",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "IAN PURPLE",
+        email: "ian.purple@example.com",
+      },
+      {
+        name: "JULIA ORANGE",
+        email: "julia.orange@example.com",
+      },
+    ],
+    _id: "006",
+    chatName: "IAN PURPLE",
+  },
+];
+module.exports = Chats;
